8411384d4b9f3de2a5098d2f5ff27778afa36e9d
Administrator <admin@Administrators-MacBook-Pro.local>
2018-07-03 11:43:47 -0500

    working on state machine for calibrating on startup

---
+++ Arduino/SandPlotter/Definitions.h
@@ -0,0 +1,7 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define STARTUP (0)
+#define CALIBRATION (1)
+
+#endif
--- Arduino/SandPlotter/SandPlotter.ino
+++ Arduino/SandPlotter/SandPlotter.ino
@@ -7,7 +7,7 @@
 #include "Arduino.h"
 #include "Stepper.h"
 #include "SimpleTimer.h"
-
+#include "Definitions.h"
 
 //Add 54 to num
 // static const uint8_t A0 = 54;
@@ -17,9 +17,9 @@
 const int innerLimit = 14;
 const int outerLimit = 15;
 const int pushButton = 14;
-const int yEnable = 56; //A2
+const int yEnable = 56;  //A2
 const int yStepPin = 60; //A6
-const int yDirPin = 61; //A7
+const int yDirPin = 61;  //A7
 // const int stepsForRevolution = 3200;
 const bool outward = true;
 const bool inward = false;
@@ -27,54 +27,80 @@ bool currentDirection = outward;
 const bool HIT = true;
 const bool NOT_HIT = false;
 
-int state = 0;
+int state = STARTUP;
 long stepCounter = 0;
 
 SimpleTimer timer;
 
-Stepper r_stepper = Stepper(yDirPin,yEnable,yStepPin);
+Stepper r_stepper = Stepper(yDirPin, yEnable, yStepPin);
 // Stepper theta_stepper = Stepper(yDirPin,yEnable,yStepPin);
 
-void setup() {
-
-  Serial.begin(9600);
-  timer.setInterval(15000, RepeatTask);
-//  pinMode(pushButton, INPUT_PULLUP);
-  pinMode(innerLimit, INPUT_PULLUP);
-  pinMode(outerLimit, INPUT_PULLUP);
-  r_stepper.Setup();
+void setup()
+{
+    Serial.begin(9600);
+    timer.setInterval(15000, RepeatTask);
+    //  pinMode(pushButton, INPUT_PULLUP);
+    pinMode(innerLimit, INPUT_PULLUP);
+    pinMode(outerLimit, INPUT_PULLUP);
+    r_stepper.Setup();
 }
 
 // function to be called repeatedly
-void RepeatTask() {
-  Serial.println("15 second timer");        
+void RepeatTask()
+{
+    Serial.println("15 second timer");
 }
 
-void ReverseDirectionOnBump(){
-  if((digitalRead(outerLimit) == HIT) && (currentDirection == outward)){
-    currentDirection = inward;
-    Serial.println(stepCounter);
-    stepCounter = 0;
-  }
-  if((digitalRead(innerLimit) == HIT) && (currentDirection == inward)){
-    currentDirection = outward;
-    Serial.println(stepCounter);
-    stepCounter = 0;
-  }
-  digitalWrite(yDirPin,currentDirection); // Enables the motor to move in a particular direction
+bool ReverseDirectionOnBump()
+{
+    bool didBump = false;
+    if ((digitalRead(outerLimit) == HIT) && (currentDirection == outward))
+    {
+        currentDirection = inward;
+        didBump = true;
+    }
+    else if ((digitalRead(innerLimit) == HIT) && (currentDirection == inward))
+    {
+        currentDirection = outward;
+        didBump = true;
+    }
+    digitalWrite(yDirPin, currentDirection); // Enables the motor to move in a particular direction
+    return didBump;
 }
 // void InitialCalibration(){
 
 // }
 
-void loop() {
-  
-  // //sets a direction and takes a step
-//  r_stepper.ChangeDirection(outward);
-  r_stepper.OneStep();
-  stepCounter+=1;
-  ReverseDirectionOnBump();
-//  Serial.println(stepCounter);
-  // SetDirection();//checks limit switches and changes direction if Hit
-  // OneStep(yStepPin,1);
+void Calibrate_R_Axis(){
+    r_stepper.OneStep();
+    stepCounter += 1;
+    if(ReverseDirectionOnBump()){
+        long lastStepCounter = stepCounter; //TODO: Declare this somewhere or keep it idno
+        Serial.println("Steps between switches = "+ String(stepCounter));
+        stepCounter = 0;
+    }
 }
+
+void loop()
+{
+    switch (state)
+    {
+    case STARTUP:
+        r_stepper.OneStep();
+        if(ReverseDirectionOnBump()) { 
+            state = CALIBRATION;
+            Serial.println("State Entering `CALIBRATION` Mode...");
+            }
+        break;
+
+    case CALIBRATION:
+        Calibrate_R_Axis();
+        
+    default:
+        state = 0;
+        Serial.println("Default Catch `state` var in unkown State: "+String(state));
+        while(true){} //stop everything here.
+
+    }//switch on state
+    
+}//loop
