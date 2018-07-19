d482bb5396e7640d4a3d818ea144830dcf63f48c
Administrator <admin@Administrators-MacBook-Pro.local>
2018-07-19 18:27:21 -0500

    adding a comment

--- Arduino/SandPlotter/Stepper.h
+++ Arduino/SandPlotter/Stepper.h
@@ -14,6 +14,9 @@ class Stepper
     void Init(int _directionPin, int _enablePin, int _stepPin);
     void Init(int _directionPin, int _enablePin, int _stepPin, int _chipSelectPin);
     void SetSpeed(int _speed);
+    //Takes a full Step
+    //currently goes at 50*speed (higher is slower)
+    //speed is microseconds between high and low triggers.
     void OneStep();
     void ChangeDirection(bool newDirection);
     void DisableMotor();
